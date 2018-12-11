446869f5c99bf4b073209316db6142ba86d3d23d
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-12-11 18:25:15 +0700

    Remove .vscode folder

--- test.cpp
+++ test.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main() {
-  cout << "TEst aja";
+  int angka = 10;
+
+  if (angka > 5) {
+    cout << "assadas";
+  } else {
+    cout << "ASdasd";
+  }
 
   return 0;
 }
